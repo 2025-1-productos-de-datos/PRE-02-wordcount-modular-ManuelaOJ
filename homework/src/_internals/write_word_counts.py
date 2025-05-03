54f31dbfa395fd7e3181fef48c8081147290f00e
ManuelaOJ <manorozcojim@unal.edu.co>
2025-05-03 12:02:30 -0500

    cambios finales en el codigo

--- homework/src/_internals/write_word_counts.py
+++ homework/src/_internals/write_word_counts.py
@@ -1,12 +1,12 @@
 import os
 
 
-def write_word_counts(counter):
-    if not os.path.exists("data/output"):
-        os.makedirs("data/output")
-
+def write_word_counts(counter, output_folder):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    output_path = os.path.join(output_folder, "wordcount.tsv")
     # save the results using tsv format
-    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
+    with open(output_path, "w", encoding="utf-8") as f:
         for key, value in counter.items():
             # write the key and value to the file
             f.write(f"{key}\t{value}\n")
